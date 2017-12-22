6ba19a6162b0bf14c0d703175e3c3e13b1729ffc
As-line <32175896+AS-line@users.noreply.github.com>
2017-12-22 17:02:35 +0300

    Add files via upload

--- peri_project/program.py
+++ peri_project/program.py
@@ -6,37 +6,47 @@ root = tkinter.Tk()
 root.title('Авторизация')
 root.geometry('420x600')
 root.resizable(False, False)
-root.config(bg='wheat')
+root.config(bg='#8090a0')
 
 
 def log_in(user_login, user_password):
+
     descriptor = sqlite3.connect('DATA.db')
     runner = descriptor.cursor()
 
     runner.execute("SELECT login, password FROM profiles_data")
     profiles = runner.fetchall()
     check_profile = (user_login, user_password)
+
     if check_profile in profiles:
         true_login = user_login
         txt_label = tkinter.Label(text='YOU HAVE SUCCESSFULLY LOGGED', width=34, bg='#0f0')
         txt_label.place(x=88.5, y=143)
-        enter_data(1, 2, 3, 4, true_login)
-
-        root.quit()
-
-        root2 = tkinter.Tk()
-        root2.title('Ввод данных')
-        root2.geometry('420x600')
-        root2.resizable(False, False)
-        root2.config(bg='wheat')
-
+        time.sleep(2)
+        root.destroy()
+        new_window()
     else:
+        
         txt_label = tkinter.Label(text='INCORRECT USERNAME OR PASSWORD', width=34, bg='red')
         txt_label.place(x=88.5, y=143)
 
     descriptor.commit()
     runner.close()
     descriptor.close()
+
+def new_window():
+    global root2
+    root2 = tkinter.Tk()
+    root2.title('Меню')
+    root2.geometry('420x600')
+    root2.resizable(False, False)
+    root2.config(bg='#8090a0')
+
+
+
+
+
+    root2.mainloop()
 
 
 def registration(new_user_name, new_user_surname, new_user_login, new_user_password):
@@ -87,13 +97,13 @@ def enter_data(user_experience, user_mind, user_diligence, user_discipline, true
 
 ############################################################################
 
-label_login = tkinter.Label(root, text='Login:', bg='wheat')
+label_login = tkinter.Label(root, text='Login:', bg='#8090a0')
 label_login.place(x=148.5, y=20)
 
 entry_login = tkinter.Entry(root)
 entry_login.place(x=148.5, y=40)
 
-label_password = tkinter.Label(root, text='Password:', bg='wheat')
+label_password = tkinter.Label(root, text='Password:', bg='#8090a0')
 label_password.place(x=148.5, y=60)
 
 entry_password = tkinter.Entry(root)
@@ -106,25 +116,25 @@ btn_log_in.place(x=143, y=110)
 ############################################################################
 
 
-label_new_name = tkinter.Label(root, text='New name:', bg='wheat')
+label_new_name = tkinter.Label(root, text='New name:', bg='#8090a0')
 label_new_name.place(x=148.5, y=180)
 
 entry_new_name = tkinter.Entry(root)
 entry_new_name.place(x=148.5, y=200)
 
-label_new_sername = tkinter.Label(root, text='New surname:', bg='wheat')
+label_new_sername = tkinter.Label(root, text='New surname:', bg='#8090a0')
 label_new_sername.place(x=148.5, y=220)
 
 entry_new_sername = tkinter.Entry(root)
 entry_new_sername.place(x=148.5, y=240)
 
-label_new_login = tkinter.Label(root, text='New login:', bg='wheat')
+label_new_login = tkinter.Label(root, text='New login:', bg='#8090a0')
 label_new_login.place(x=148.5, y=280)
 
 entry_new_login = tkinter.Entry(root)
 entry_new_login.place(x=148.5, y=300)
 
-label_new_passw = tkinter.Label(root, text='New password:', bg='wheat')
+label_new_passw = tkinter.Label(root, text='New password:', bg='#8090a0')
 label_new_passw.place(x=148.5, y=330)
 
 entry_new_passw = tkinter.Entry(root)
@@ -136,3 +146,4 @@ btn_reg.bind('<Button-1>', lambda event: registration(entry_new_name.get(), entr
 btn_reg.place(x=143, y=380)
 
 root.mainloop()
+
